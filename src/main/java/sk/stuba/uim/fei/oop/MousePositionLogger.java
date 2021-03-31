4c5ab7fe7acd6b6e94c18819f04efe6e961d08d8
Theodora2000 <xsimont@stuba.sk>
2021-04-01 00:14:57 +0200

    MouseAdapter

---
+++ src/main/java/sk/stuba/uim/fei/oop/MousePositionLogger.java
@@ -0,0 +1,12 @@
+package sk.stuba.uim.fei.oop;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+public class MousePositionLogger extends MouseAdapter {
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        super.mouseClicked(e);
+    }
+}
--- src/main/java/sk/stuba/uim/fei/oop/Okno.java
+++ src/main/java/sk/stuba/uim/fei/oop/Okno.java
@@ -2,12 +2,20 @@ package sk.stuba.uim.fei.oop;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-public class Okno extends JFrame {
+public class Okno extends JFrame{
     public Okno()throws HeadlessException{
         super();
         this.setVisible(true);
         this.setSize(300,300);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println(e.getPoint().toString());
+            }
+        });
     }
 }
