e75555ef1b83d4d5f01f4f000f5d0642023b4c8f
Theodora2000 <xsimont@stuba.sk>
2021-04-01 00:01:50 +0200

    default close

--- src/main/java/sk/stuba/uim/fei/oop/Main.java
+++ src/main/java/sk/stuba/uim/fei/oop/Main.java
@@ -4,15 +4,7 @@ import java.util.List;
 
 public class Main {
     public static void main(String []args){
-        Cloveka c = new Cloveka();
-        c.getName();
-        c.setAdresa("adresa");
-        c.setName("meno");
-        c.setVaha(80.0);
-        c.setVek(20);
-        c.setKamarati(List.of(new Cloveka()));
-
-        System.out.println(c.toString());
+        new Okno();
     }
 
 }
---
+++ src/main/java/sk/stuba/uim/fei/oop/Okno.java
@@ -0,0 +1,13 @@
+package sk.stuba.uim.fei.oop;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Okno extends JFrame {
+    public Okno()throws HeadlessException{
+        super();
+        this.setVisible(true);
+        this.setSize(300,300);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
